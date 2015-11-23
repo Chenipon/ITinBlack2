9c3cf3cc5bfc88c81f50fe6a64568a8449db1391
McJeffr <jeffrey.lingen@hotmail.com>
2015-11-23 11:39:42 +0100

    Finished Luggage Overview Table + Search function

--- src/Fys/Controllers/LuggageOverviewController.java
+++ src/Fys/Controllers/LuggageOverviewController.java
@@ -1,9 +1,13 @@
 package Fys.Controllers;
 
 import Fys.Models.User;
+import Fys.Views.ViewModels.AccountTabelView;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,8 +17,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import Fys.Views.ViewModels.LuggageTabelView;
 
 /**
  * FXML Controller class. This class controls the Luggage Overview screen
@@ -26,14 +34,31 @@ public class LuggageOverviewController implements Initializable {
 
     public static User currentUser;
 
-    @FXML private Label lblUsername;
-    @FXML private Label lblErrorMessage;
+    @FXML private Label lblUsername, lblErrorMessage;
+    @FXML private TableView tblLuggage;
+    @FXML private TableColumn colType, colBrand, colMaterial, colColor, colComment, colStatus;
     @FXML private MenuButton ddwnLuggageType;
-    @FXML private TextField fieldLuggageSearchBox;
+    @FXML private TextField lblSearch;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         lblUsername.setText(currentUser.getUsername());
+        colType.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("type"));
+        colBrand.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("brand"));
+        colMaterial.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("material"));
+        colColor.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("color"));
+        colComment.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("comment"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<AccountTabelView, String>("status"));
+        try {
+            tblLuggage.setItems(getLuggageList());
+        } catch (Exception ex) {
+            Logger.getLogger(LuggageOverviewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public ObservableList<LuggageTabelView> getLuggageList() throws Exception{
+        ObservableList<LuggageTabelView> luggageList= new LuggageTabelView().getLuggageList();
+        return luggageList;
     }
 
     @FXML
@@ -53,21 +78,21 @@ public class LuggageOverviewController implements Initializable {
         ddwnLuggageType.setText("Connected");
         ddwnLuggageType.setPrefWidth(110);
     }
+    
+    @FXML
+    private void ddwnLuggageAllEvent(ActionEvent event) {
+        ddwnLuggageType.setText("All");
+        ddwnLuggageType.setPrefWidth(110);
+    }
 
     @FXML
-    private void btnSearchLuggageEvent(ActionEvent event) {
-        if (!fieldLuggageSearchBox.getText().equals("")) {
-            if (!ddwnLuggageType.getText().equals("Luggage Type")) {
-                lblErrorMessage.setText("");
-                //MYSQL SEARCH QUERY IN HERE
-                //LOAD TABLE METHODS IN HERE
-                System.out.println("Search Luggage type " + ddwnLuggageType.getText()
-                        + " with query: " + fieldLuggageSearchBox.getText());
-            } else {
-                lblErrorMessage.setText("Select a luggage type");
-            }
+    private void btnSearchLuggageEvent(ActionEvent event) throws Exception {
+        if (!ddwnLuggageType.getText().equals("Luggage Type")) {
+            lblErrorMessage.setText("");
+            ObservableList<LuggageTabelView> luggageList = new LuggageTabelView().getLuggageList(lblSearch.getText(), ddwnLuggageType.getText());
+            tblLuggage.setItems(luggageList);
         } else {
-            lblErrorMessage.setText("Search term cannot be empty");
+            lblErrorMessage.setText("Select a luggage type");
         }
     }
 
---
+++ src/Fys/Views/ViewModels/LuggageTabelView.java
@@ -0,0 +1,123 @@
+package Fys.Views.ViewModels;
+
+import Fys.Models.Status;
+import Fys.Tools.ConnectMysqlServer;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Jeffrey van der Lingen, IS106-2
+ */
+public class LuggageTabelView {
+
+    private String type;
+    private String brand;
+    private String material;
+    private String color;
+    private String comment;
+    private String status;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ObservableList<LuggageTabelView> getLuggageList() throws Exception {
+        ObservableList<LuggageTabelView> luggage = FXCollections.observableArrayList();
+        try (Connection db = new ConnectMysqlServer().dbConnect()) {
+            Statement statement = db.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM luggage");
+            while (result.next()) {
+                LuggageTabelView foundLuggage = new LuggageTabelView();
+                foundLuggage.setType(result.getString(2));
+                foundLuggage.setBrand(result.getString(3));
+                foundLuggage.setMaterial(result.getString(4));
+                foundLuggage.setColor(result.getString(5));
+                foundLuggage.setComment(result.getString(6));
+                foundLuggage.setStatus(new Status().getStatusById(result.getInt(8)).getStatusName());
+                luggage.add(foundLuggage);
+            }
+        }
+        return luggage;
+    }
+
+    public ObservableList<LuggageTabelView> getLuggageList(String searchTerm, String status) throws Exception {
+        ObservableList<LuggageTabelView> luggage = FXCollections.observableArrayList();
+        try (Connection db = new ConnectMysqlServer().dbConnect()) {
+            Status statusId = new Status().getStatusByName(status);
+            Statement statement = db.createStatement();
+            ResultSet result;
+            if (statusId.getId() == 0) {
+                result = statement.executeQuery("SELECT * FROM luggage WHERE type LIKE '%"
+                        + searchTerm + "%' OR brand LIKE '%" + searchTerm + "%' OR material LIKE '%"
+                        + searchTerm + "%' OR color LIKE '%" + searchTerm + "%' OR comments LIKE '%"
+                        + searchTerm + "%'");
+            } else if (searchTerm.equals("")) {
+                result = statement.executeQuery("SELECT * FROM luggage WHERE statusid=" + statusId.getId());
+            } else {
+                result = statement.executeQuery("SELECT * FROM luggage WHERE (type LIKE '%"
+                        + searchTerm + "%' OR brand LIKE '%" + searchTerm + "%' OR material LIKE '%"
+                        + searchTerm + "%' OR color LIKE '%" + searchTerm + "%' OR comments LIKE '%"
+                        + searchTerm + "%') AND statusid=" + statusId.getId());
+            }
+            while (result.next()) {
+                LuggageTabelView foundLuggage = new LuggageTabelView();
+                foundLuggage.setType(result.getString(2));
+                foundLuggage.setBrand(result.getString(3));
+                foundLuggage.setMaterial(result.getString(4));
+                foundLuggage.setColor(result.getString(5));
+                foundLuggage.setComment(result.getString(6));
+                foundLuggage.setStatus(new Status().getStatusById(result.getInt(8)).getStatusName());
+                luggage.add(foundLuggage);
+            }
+        }
+        return luggage;
+    }
+}
