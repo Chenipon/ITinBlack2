e3a0ac9c816c446ad5b88f67d9e49b6a99700d30
McJeffr <jeffrey.lingen@hotmail.com>
2015-11-24 11:32:06 +0100

    Adjusted a few comments

--- src/Fys/Tools/ConnectMysqlServer.java
+++ src/Fys/Tools/ConnectMysqlServer.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Fys.Tools;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 /**
  *
- * @author Daan
+ * @author Daan Befort, IS106-2
  */
 public class ConnectMysqlServer {
     private Connection connection;
